9efaf14cde417418a441afb2a7a9dc3886eb5dec
Jonathan Eberle <jeberle@student.42heilbronn.de>
2025-04-08 21:05:52 +0200

    reset ...

--- srcs/requirements/nginx/tools/init-server.sh
+++ srcs/requirements/nginx/tools/init-server.sh
@@ -3,8 +3,6 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/nginx/ssl/nginx.key \
 	-out /etc/nginx/ssl/nginx.crt \
-	-subj "/C=XX/ST=State/L=City/O=Organization/OU=Unit/CN=${DOMAIN_NAME}"
+	-subj "/C=XX/ST=State/L=City/O=Organization/OU=Unit/CN=localhost"
 
-envsubst '${DOMAIN_NAME}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-exec nginx -g 'daemon off;'
+nginx -g "daemon off;"
